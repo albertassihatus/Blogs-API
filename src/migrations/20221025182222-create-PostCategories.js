701aa7c696befd8b8304b8409b1a0a466780fe0e
Hatus Albertassi <hatus.albertassi@gmail.com>
2022-10-26 10:59:38 -0300

    req11

--- src/migrations/20221025182222-create-PostCategories.js
+++ src/migrations/20221025182222-create-PostCategories.js
@@ -4,9 +4,9 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('posts_categories', {
       post_id: {
+        type: Sequelize.INTEGER,
         allowNull: false,
         primaryKey: true,
-        type: Sequelize.INTEGER,
         references: {
           model: 'blog_posts',
           key: 'id'
